80c6b41c0150dc367f5d09ab329d5285e90617ae
Vasīlijs Dvils-Dmitrijevs <68693904+eyfect@users.noreply.github.com>
2023-03-20 00:52:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,73 @@
 # python3
+import threading
+
+class PriorityQueue:
+    def __init__(self, n):
+        self.heap = [(0, i) for i in range(n)]
+        self.size = n
+        self.lock = threading.Lock()
+
+    def push(self, item, priority):
+        with self.lock:
+            self.heap.append((priority, item))
+            self.size += 1
+            self._siftup(self.size - 1)
+
+    def pop(self):
+        with self.lock:
+            lastelt = self.heap.pop()
+            self.size -= 1
+            if self.heap:
+                returnitem = self.heap[0]
+                self.heap[0] = lastelt
+                self._siftdown(0)
+            else:
+                returnitem = lastelt
+            return returnitem
+
+    def _siftup(self, pos):
+        if pos > 0:
+            parentpos = (pos - 1) >> 1
+            if self.heap[pos][0] < self.heap[parentpos][0]:
+                self.heap[pos], self.heap[parentpos] = self.heap[parentpos], self.heap[pos]
+                self._siftup(parentpos)
+
+    def _siftdown(self, pos):
+        childpos = (pos << 1) + 1
+        if childpos < self.size:
+            rightpos = childpos + 1
+            if rightpos < self.size and not self.heap[childpos][0] < self.heap[rightpos][0]:
+                childpos = rightpos
+            if not self.heap[pos][0] < self.heap[childpos][0]:
+                self.heap[pos], self.heap[childpos] = self.heap[childpos], self.heap[pos]
+                self._siftdown(childpos)
 
 def parallel_processing(n, m, data):
     output = []
-    thread = [0] * n
+    pq = PriorityQueue(n)
 
-    for i in range(m):
-        threadnxt = 0
-        for j in range(1, n):
-            if thread[j] < thread[threadnxt]:
-                threadnxt = j
-        x = thread[threadnxt]
-        y = x + data[i]
-        thread[threadnxt] = y
-        output.append((threadnxt, x))
+    def process(i):
+        t = 0
+        while True:
+            item = pq.pop()
+            with pq.lock:
+                if item[0] > t:
+                    pq.push(item[1], item[0])
+                    continue
+                output.append((i, t))
+                t += data[i]
+                if i + n < m:
+                    pq.push(i + n, t)
+                break
+
+    threads = [threading.Thread(target=process, args=(i,)) for i in range(n)]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
     return output
+
 
 def main():
     n, m = map(int, input().split())
@@ -23,3 +77,7 @@ def main():
 
     for i in range(m):
         print(result[i][0], result[i][1])
+
+
+if __name__ == "__main__":
+    main()
